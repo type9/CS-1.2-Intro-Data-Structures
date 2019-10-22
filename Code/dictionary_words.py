e2183a7e7573441dbf5b760004db4fc583a70970
type9 <gabespersonal@gmail.com>
2019-10-22 13:19:26 -0700

    Added comments

--- Code/dictionary_words.py
+++ Code/dictionary_words.py
@@ -2,7 +2,6 @@ import random
 import sys
 from rearrange import rearrange
 
-DEBUG = True
 def random_sentence(num_words):
     dictionary = open('/usr/share/dict/words', 'r')
     dictionary_list = list()
@@ -10,13 +9,13 @@ def random_sentence(num_words):
     word_list = list() # final list of words
 
     num_lines = 0 # total word count
-    for line in dictionary:
+    for line in dictionary: # converts each line of the dictionary to an array
         num_lines += 1
         dictionary_list.append(line)
 
-    rand_indexes = gen_rand_indexes(num_lines, num_words)
+    rand_indexes = gen_rand_indexes(num_lines, num_words) # generates a random index for the number of random words we need
 
-    for index in rand_indexes:
+    for index in rand_indexes: # for each random index generated, append a word at that index
         word = dictionary_list[index][:-2] # slices off new line char (last 2 indexes)
         word_list.append(word)
     
@@ -24,10 +23,10 @@ def random_sentence(num_words):
 
 def gen_rand_indexes(num_lines, num_indexes):
     indexes = list()
-    for i in range(num_indexes): # for the number of idexes 
+    for i in range(num_indexes): # for the number of idexes append a random index
         index = random.randint(0, num_lines)
         while index in indexes: # keeps randomizing so we don't have repeated indexes
-            index = random.randint(0, num_lines)
+            index = random.randint(0, num_lines) # rerandomizes if we already have that index
         indexes.append(index)
     
     return indexes
