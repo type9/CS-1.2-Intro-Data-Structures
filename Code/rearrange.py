95e67308494ccd166bf050cb0b48f8eb60409ef2
type9 <gabespersonal@gmail.com>
2019-10-21 17:02:23 -0700

    Added and finished rearrange.py

---
+++ Code/rearrange.py
@@ -0,0 +1,28 @@
+import random
+import sys
+
+DEBUG = False
+
+words = list()
+
+if DEBUG:
+    words = ['car', 'bar', 'star']
+
+def rearrange(my_list):
+    for i in range(len(my_list)): # for every list element
+        temp = my_list[i] # temp preparring for swap
+
+        rand_index = random.randint(0, (len(my_list)-1)) # choose a random index
+
+        my_list[i] = my_list[rand_index] # swap their places
+        my_list[rand_index] = temp
+    return my_list
+
+if __name__=='__main__':
+    words = sys.argv[1:]
+
+    if DEBUG:
+        print(rearrange(words))
+
+    print(rearrange(words))
+    
