c50c59c6df631027bfe1385b95ad6e8ff1a39616
type9 <gabespersonal@gmail.com>
2019-12-04 13:29:59 -0800

    Added annotations for runtimes in hashtable.py

--- Code/hashtable.py
+++ Code/hashtable.py
@@ -37,7 +37,7 @@ class HashTable(object):
 
     def values(self):
         """Return a list of all values in this hash table.
-        TODO: Running time: O(???) Why and under what conditions?"""
+        TODO: Running time: O(n) Has to loop through buckets and each item in each bucket"""
         # TODO: Loop through all buckets
         # TODO: Collect all values in each bucket
         values = []
@@ -48,7 +48,7 @@ class HashTable(object):
 
     def items(self):
         """Return a list of all items (key-value pairs) in this hash table.
-        TODO: Running time: O(???) Why and under what conditions?"""
+        TODO: Running time: O(n) Has to loop through buckets and each item in each bucket"""
         # Collect all pairs of key-value entries in each bucket
         all_items = []
         for bucket in self.buckets:
@@ -57,7 +57,7 @@ class HashTable(object):
 
     def length(self):
         """Return the number of key-value entries by traversing its buckets.
-        TODO: Running time: O(???) Why and under what conditions?"""
+        TODO: Running time: O(n) Has to loop through buckets and each item in each bucket"""
         # TODO: Loop through all buckets
         # TODO: Count number of key-value entries in each bucket
         count = 0
@@ -68,7 +68,7 @@ class HashTable(object):
 
     def contains(self, key):
         """Return True if this hash table contains the given key, or False.
-        TODO: Running time: O(???) Why and under what conditions?"""
+        TODO: Running time: O(n) Has to check the list of keys to see if it matches our condition key"""
         # TODO: Find bucket where given key belongs
         # TODO: Check if key-value entry exists in bucket
         if key in self.keys():
@@ -78,7 +78,7 @@ class HashTable(object):
 
     def get(self, key):
         """Return the value associated with the given key, or raise KeyError.
-        TODO: Running time: O(???) Why and under what conditions?"""
+        TODO: Running time: O(1) in which the hash has generated no collissions"""
         # TODO: Find bucket where given key belongs
         # TODO: Check if key-value entry exists in bucket
         # TODO: If found, return value associated with given key
@@ -98,7 +98,7 @@ class HashTable(object):
 
     def set(self, key, value):
         """Insert or update the given key with its associated value.
-        TODO: Running time: O(???) Why and under what conditions?"""
+        TODO: Running time: O(1) in which the hash has generated no collissions"""
         # TODO: Find bucket where given key belongs
         # TODO: Check if key-value entry exists in bucket
         # TODO: If found, update value associated with given key
@@ -115,7 +115,7 @@ class HashTable(object):
 
     def delete(self, key):
         """Delete the given key from this hash table, or raise KeyError.
-        TODO: Running time: O(???) Why and under what conditions?"""
+        TODO: Running time: O(1) in which the hash has generated no collissions"""
         # TODO: Find bucket where given key belongs
         # TODO: Check if key-value entry exists in bucket
         # TODO: If found, delete entry associated with given key
